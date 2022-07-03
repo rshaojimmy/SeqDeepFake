4308492b051477531b6db66e0ef14b9e58754dcb
wtx-mmlab <tianxing.wu@ntu.edu.sg>
2022-07-03 18:48:39 +0800

    edit shell script

--- test.sh
+++ test.sh
@@ -1,22 +1,25 @@
 LOGNAME='YOUR-LOGNAME-HERE'
+CONFIG='./configs/r50.json'
+DATA_DIR='YOUR-DATASET-ROOT-HERE'
+DATASET_NAME='facial_attributes'
+TEST_TYPE='adaptive'
+RESULTS_DIR='./results'
 
 HOST='127.0.0.1'
 PORT='12345'
-
 NUM_GPU=1
 
 python test.py \
---cfg './configs/r50.json' \
---data_dir 'YOUR-DATASET-ROOT-HERE' \
---dataset_name 'facial_attributes' \
---test_type 'adaptive' \
---launcher pytorch \
---rank 0 \
 --log_name ${LOGNAME} \
+--cfg ${CONFIG} \
+--data_dir ${DATA_DIR} \
+--dataset_name ${DATASET_NAME} \
+--test_type ${TEST_TYPE} \
 --dist-url tcp://${HOST}:${PORT} \
---world_size $NUM_GPU \
---results_dir './results'
+--world_size ${NUM_GPU} \
+--rank 0 \
+--launcher none \
+--results_dir ${RESULTS_DIR}
 
 # dataset_name: facial_components facial_attributes
 # test_type: fixed adaptive
-
--- train.sh
+++ train.sh
@@ -1,20 +1,23 @@
 LOGNAME=log$(date +"%Y%m%d_%H%M%S")
+CONFIG='./configs/r50.json'
+DATA_DIR='YOUR-DATASET-ROOT-HERE'
+DATASET_NAME='facial_attributes'
+RESULTS_DIR='./results'
 
 HOST='127.0.0.1'
 PORT='12345'
-
-NUM_GPU=4
+NUM_GPU=1
 
 python train.py \
---cfg './configs/r50.json' \
---data_dir '/mnt/lustre/share/txwu/data/Seq-DeepFake' \
---dataset_name 'facial_attributes' \
+--log_name ${LOGNAME} \
+--cfg ${CONFIG} \
+--data_dir ${DATA_DIR} \
+--dataset_name ${DATASET_NAME} \
 --val_epoch 10 \
 --model_save_epoch 5 \
 --manual_seed 777 \
---launcher pytorch \
---rank 0 \
---log_name ${LOGNAME} \
 --dist-url tcp://${HOST}:${PORT} \
 --world_size $NUM_GPU \
---results_dir './results'
+--rank 0 \
+--launcher pytorch \
+--results_dir ${RESULTS_DIR}
